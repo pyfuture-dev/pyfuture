2da27fee899904d7a442bd4111f37a3b98e94f73
gouzi <530971494@qq.com>
2023-10-19 18:49:44 +0800

    clean code

--- examples/pep622/ex_match.py
+++ examples/pep622/ex_match.py
@@ -16,35 +16,42 @@ match i:
 """)
 
 
-def orelse(convert_if: any, subject: ast.Name, value: any, body: ast.Expr):
+# TODO(gouzil): Value can be replaced with the specified type
+def generate_orelse(convert_if: ast.If, subject: ast.Name, value: any, body: ast.Expr):
     if len(convert_if.orelse) == 0:
-        if isinstance(value, ast.MatchValue):
-            """
-            case "test":
-            """
-            convert_if.orelse = [ast.If(test=ast.Compare(left=subject, ops=[ast.Is()], comparators=[value.value]), body=body, orelse=[])]
-        elif isinstance(value, ast.MatchAs):
-            """
+        match value:
+            case ast.MatchValue:
+                """
+                case "test":
+                """
+                convert_if.orelse = [ast.If(test=ast.Compare(left=subject, ops=[ast.Is()], comparators=[value.value]), body=body, orelse=[])]
+            case ast.MatchAs:
+                """
+                case _:
+                """
+                # TODO(gouzil): case [x] if x>0
+                convert_if.orelse = [body]
             case _:
-            """
-            # TODO(gouzil): case [x] if x>0
-            convert_if.orelse = [body]
+                RuntimeError(f"Not currently supported {value} type")
     else:
-        orelse(convert_if.orelse[0], subject, value, body)
+        generate_orelse(convert_if.orelse[0], subject, value, body)
 
 for index, node in enumerate(match_func.body):
     if isinstance(node, ast.Match):
         # cache the 0th case
         zero_case = match_func.body[index].cases[0]
+
+        """
+        match value:
+            case _:
+        """
         if len(node.cases) == 1 and isinstance(zero_case.pattern, ast.MatchAs) and zero_case.pattern.pattern is None:
             convert_if = zero_case.body
         else:
             # Initialize `If`` Root Node
             convert_if = ast.If(test=ast.Compare(left=match_func.body[index].subject , ops=[ast.Is()], comparators=[zero_case.pattern.value]), body=zero_case.body, orelse=[])
-            for case_index, case_node in enumerate(node.cases):
-                if case_index == 0:
-                    continue
-                orelse(convert_if, match_func.body[index].subject, case_node.pattern, case_node.body)
+            for case_index, case_node in enumerate(node.cases, 1):
+                generate_orelse(convert_if, match_func.body[index].subject, case_node.pattern, case_node.body)
                 # breakpoint()
         # print(ast.dump(convert_if))
         # breakpoint()
