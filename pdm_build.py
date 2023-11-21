04f04ce0d0dca739f3b37fef711a7f6d4e440464
Zhan Rongrui <2742392377@qq.com>
2023-11-21 19:16:52 +0800

    update workflow

--- pdm_build.py
+++ pdm_build.py
@@ -1,31 +1,16 @@
 from pdm.backend.base import Context
+from pdm.backend.wheel import WheelBuilder
 from pathlib import Path
 from typing import Any
-from pyfuture.utils import transfer_file
+import sys
 
-
-def hook_config(context: Context) -> dict[str, Any]:
-    return (
-        context.config.data.get("tool", {})
-        .get("pdm", {})
-        .get("build", {})
-        .get("hooks", {})
-        .get("pyfuture", {})
-    )
 
 def pdm_build_hook_enabled(context: Context):
     return context.target == "wheel"
 
-def pdm_build_update_files(context: Context, files: dict[str, Path]) -> None:
-    build_dir = context.ensure_build_dir()
-    package_dir = Path(context.config.build_config.package_dir)
-    includes = context.config.build_config.includes
-    for include in includes:
-        src_path = package_dir/include
-        tgt_path = build_dir/include
-        for src_file in src_path.glob("**/*.py"):
-            tgt_file = tgt_path/src_file.relative_to(src_path)
-            files[f"{tgt_path.relative_to(build_dir)}"] = tgt_file
-            # TODO: support config target
-            transfer_file(src_file, tgt_file, target=(3, 8))
+def pdm_build_finalize(context: Context, artifact: Path) -> None:
+    if isinstance(context.builder, WheelBuilder):
+        name_version = context.builder.name_version
+        tag = context.builder.tag.replace("py3", f"py{sys.version_info.major}{sys.version_info.minor}")
+        artifact.rename(artifact.parent/f"{name_version}-{tag}.whl")
                     
