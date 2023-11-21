59449a9763ba99cba0f3d02294795bd4969b4b1c
Zhan Rongrui <2742392377@qq.com>
2023-11-21 23:33:22 +0800

    improve

--- pdm_build.py
+++ pdm_build.py
@@ -1,16 +1,13 @@
 from pdm.backend.base import Context
-from pdm.backend.wheel import WheelBuilder
-from pathlib import Path
-from typing import Any
 import sys
 
 
 def pdm_build_hook_enabled(context: Context):
     return context.target == "wheel"
 
-def pdm_build_finalize(context: Context, artifact: Path) -> None:
-    if isinstance(context.builder, WheelBuilder):
-        name_version = context.builder.name_version
-        tag = context.builder.tag.replace("py3", f"py{sys.version_info.major}{sys.version_info.minor}")
-        artifact.rename(artifact.parent/f"{name_version}-{tag}.whl")
-                    
+
+def pdm_build_initialize(context: Context) -> None:
+    config_settings = context.builder.config_settings
+    config_settings[
+        "--python-tag"
+    ] = f"py{sys.version_info.major}{sys.version_info.minor}"
