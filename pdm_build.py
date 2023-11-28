1d9155345be444a6852d5ec2e740439cdddc6e84
Zhan Rongrui <2742392377@qq.com>
2023-11-28 21:41:28 +0800

    fix hook bug

--- pdm_build.py
+++ pdm_build.py
@@ -17,7 +17,7 @@ def pdm_build_update_files(context: Context, files: dict[str, Path]) -> None:
         tgt_path = build_dir / include
         for src_file in src_path.glob("**/*.py"):
             tgt_file = tgt_path / src_file.relative_to(src_path)
-            files[f"{tgt_path.relative_to(build_dir)}"] = tgt_file
+            files[f"{tgt_file.relative_to(build_dir)}"] = tgt_file
             # TODO: support specific version target
             # transfer_file(src_file, tgt_file, target=sys.version_info[:2])
             transfer_file(src_file, tgt_file, target=(3, 8))
