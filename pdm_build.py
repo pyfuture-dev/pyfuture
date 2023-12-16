101ccc50bc013bb2967321d209a9ea1c89cb21c2
zrr1999 <2742392377@qq.com>
2023-12-16 15:08:07 +0800

    ♻️ refactor: use pdm-pyfuture

--- pdm_build.py
+++ pdm_build.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import os
 import sys
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -11,25 +10,8 @@ if TYPE_CHECKING:
 
 def pdm_build_hook_enabled(context: Context):
     if sys.version_info[:2] < (3, 12):
-        raise RuntimeError("pyfuture cannot be installed from source for Python < 3.12")
+        raise RuntimeError("PyFuture cannot be installed from source for Python < 3.12")
     if context.target == "editable" and not os.path.exists(".pdm-python"):
         with open(".pdm-python", "w") as f:
             f.write(sys.executable)
-    return context.target == "wheel"
-
-
-def pdm_build_update_files(context: Context, files: dict[str, Path]) -> None:
-    from pyfuture.utils import transfer_file
-
-    build_dir = context.ensure_build_dir()
-    package_dir = Path(context.config.build_config.package_dir)
-    includes = context.config.build_config.includes
-    for include in includes:
-        src_path = package_dir / include
-        tgt_path = build_dir / include
-        for src_file in src_path.glob("**/*.py"):
-            tgt_file = tgt_path / src_file.relative_to(src_path)
-            files[f"{tgt_file.relative_to(build_dir)}"] = tgt_file
-            # TODO: support specific version target
-            # transfer_file(src_file, tgt_file, target=sys.version_info[:2])
-            transfer_file(src_file, tgt_file, target=(3, 8))
+    return False
