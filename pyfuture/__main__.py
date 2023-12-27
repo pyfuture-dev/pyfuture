d0b185281a96cbfc9a7cfb46db36e9a0c496f19c
zrr1999 <2742392377@qq.com>
2023-12-27 14:53:27 +0800

    💄 style: improve log

--- pyfuture/__main__.py
+++ pyfuture/__main__.py
@@ -4,37 +4,39 @@ from pathlib import Path
 
 import typer
 from loguru import logger
+from rich.console import Console
+from rich.highlighter import NullHighlighter
+from rich.logging import RichHandler
+from rich.style import Style
 
 from pyfuture.utils import transfer_file
 
 app = typer.Typer()
 
 
+def init_logger(log_level: str):
+    handler = RichHandler(console=Console(style=Style()), highlighter=NullHighlighter(), markup=True)
+    logger.remove()
+    logger.add(handler, format="{message}", level=log_level)
+
+
 @app.command()
-def transfer(
-    src_file: Path,
-    tgt_file: Path,
-    *,
-    target: str = "3.8",
-):
+def transfer(src_file: Path, tgt_file: Path, *, target: str = "3.8", log_level: str = "INFO"):
     """
     Transfer code from src_file and write to tgt_file.
     """
     assert target == "3.8", "PyFuture is very early stage, not support target argument yet"
+    init_logger(log_level)
     transfer_file(src_file, tgt_file)
 
 
 @app.command()
-def watch(
-    src_file: Path,
-    tgt_file: Path,
-    *,
-    target: str = "3.8",
-):
+def watch(src_file: Path, tgt_file: Path, *, target: str = "3.8", log_level: str = "INFO"):
     """
     Transfer all python files in src_dir to build_dir, and watch for changes.
     """
     assert target == "3.8", "PyFuture is very early stage, not support target argument yet"
+    init_logger(log_level)
     transfer_file(src_file, tgt_file)
 
     from watchfiles import Change, watch
@@ -51,16 +53,12 @@ def watch(
 
 
 @app.command()
-def transfer_dir(
-    src_dir: Path,
-    build_dir: Path,
-    *,
-    target: str = "3.8",
-):
+def transfer_dir(src_dir: Path, build_dir: Path, *, target: str = "3.8", log_level: str = "INFO"):
     """
     Transfer all python files in src_dir to build_dir.
     """
     assert target == "3.8", "PyFuture is very early stage, not support target argument yet"
+    init_logger(log_level)
 
     for src_file in src_dir.glob("**/*.py"):
         tgt_file = build_dir / src_file.relative_to(src_dir)
@@ -68,16 +66,12 @@ def transfer_dir(
 
 
 @app.command()
-def watch_dir(
-    src_dir: Path,
-    build_dir: Path,
-    *,
-    target: str = "3.8",
-):
+def watch_dir(src_dir: Path, build_dir: Path, *, target: str = "3.8", log_level: str = "INFO"):
     """
     Transfer all python files in src_dir to build_dir, and watch for changes.
     """
     assert target == "3.8", "PyFuture is very early stage, not support target argument yet"
+    init_logger(log_level)
     transfer_dir(src_dir, build_dir, target=target)
 
     from watchfiles import Change, PythonFilter, watch
