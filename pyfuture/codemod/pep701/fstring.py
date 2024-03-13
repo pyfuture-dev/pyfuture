86a917b9d62a0112b38cee0b72f8e0a47ac7d25e
zrr1999 <2742392377@qq.com>
2024-03-13 12:46:55 +0800

    🧹 chore: remove unused method `remove_type_parameters` in `fstring.py`

--- pyfuture/codemod/pep701/fstring.py
+++ pyfuture/codemod/pep701/fstring.py
@@ -4,23 +4,14 @@ from typing import Any
 
 import libcst as cst
 from libcst import (
-    Arg,
     ClassDef,
     FunctionDef,
-    Index,
-    Name,
-    SimpleStatementLine,
-    Subscript,
-    SubscriptElement,
 )
 from libcst.codemod import (
     CodemodContext,
     VisitorBasedCodemodCommand,
 )
-from libcst.codemod.visitors import AddImportsVisitor
 from libcst.metadata import ScopeProvider
-
-from ..utils import gen_type_param
 
 
 class TransformFStringCommand(VisitorBasedCodemodCommand):
@@ -58,43 +49,6 @@ class TransformFStringCommand(VisitorBasedCodemodCommand):
     def __init__(self, context: CodemodContext) -> None:
         self.node_to_wrapper: dict[FunctionDef | ClassDef, Any] = {}
         super().__init__(context)
-
-    def remove_type_parameters[T: FunctionDef | ClassDef](
-        self, node: T, prefix: str = "", suffix: str = ""
-    ) -> tuple[list[SimpleStatementLine], T]:
-        type_params = node.type_parameters
-        if type_params is None:
-            return [], node
-        statements = []
-        new_node = node.with_changes(type_parameters=None)
-
-        slices = []
-        for type_param in type_params.params:
-            new_name = type_param.param.name.with_changes(value=f"{prefix}{type_param.param.name.value}{suffix}")
-
-            AddImportsVisitor.add_needed_import(self.context, "typing", type_param.param.__class__.__name__)
-            statements.append(gen_type_param(type_param.param, new_name))
-            slices.append(
-                SubscriptElement(
-                    slice=Index(value=new_name),
-                ),
-            )
-
-        if isinstance(new_node, ClassDef) and slices:
-            AddImportsVisitor.add_needed_import(self.context, "typing", "Generic")
-            generic_base = Arg(
-                value=Subscript(
-                    value=Name(
-                        value="Generic",
-                        lpar=[],
-                        rpar=[],
-                    ),
-                    slice=slices,
-                )
-            )
-            new_node = new_node.with_changes(bases=[*new_node.bases, generic_base])
-
-        return statements, new_node
 
     def leave_FormattedString(self, original_node: cst.FormattedString, updated_node: cst.FormattedString):
         expressions = []
